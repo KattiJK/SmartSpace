337cef054910339708ffefc3718e7e195bbe0ca0
Ekaterina <103816494+KattiJK@users.noreply.github.com>
2023-03-13 18:35:42 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,44 @@
+/*Burger*/
+
+document.addEventListener('click', documentClick);
+
+function documentClick(e){
+   const targetItem = e.target;
+   if (targetItem.closest('.icon-menu')){
+      document.documentElement.classList.toggle('menu-open');
+   }
+}
+
+
+const swiper = new Swiper('.slider-main-block', {
+	// Optional parameters
+	loop: true,
+	// Navigation arrows
+	navigation: {
+		nextEl: '.body-main-block__arrow.swiper-button-next',
+		prevEl: '.body-main-block__arrow.swiper-button-prev',
+	},
+});
+
+// Таби
+const tabNavItems = document.querySelectorAll('.tabs-deals__button');
+const tabItems = document.querySelectorAll('.item-tabs');
+document.addEventListener("click", function (e) {
+	const targetElement = e.target;
+	let currentActiveIndex = null;
+	let newActiveIndex = null;
+	if (targetElement.closest('.tabs-deals__button')) {
+		tabNavItems.forEach((tabNavItem, index) => {
+			if (tabNavItem.classList.contains('active')) {
+				currentActiveIndex = index;
+				tabNavItem.classList.remove('active');
+			}
+			if (tabNavItem === targetElement) {
+				newActiveIndex = index;
+			}
+		});
+		targetElement.classList.add('active');
+		tabItems[currentActiveIndex].classList.remove('active');
+		tabItems[newActiveIndex].classList.add('active');
+	}
+});
